24bfe06c078f25b2454c9b33c065ca7491ec072e
Yana Kormshchikova <veryutina.yana@yandex.ru>
2023-09-12 16:33:56 +0300

    minor changes

--- task_2.py
+++ task_2.py
@@ -4,14 +4,14 @@ class Tester:
         self.name = name
         self.deadline = deadline
 
-    def work_hard(self):
-        if self.deadline:
+    def work_hard(self, deadline=True):
+        if deadline:git
             print(self.name, 'Что ж, ещё часок поработаю!')
         else:
             print(self.name, 'Можно отдыхать')
 
 
-tester_1 = Tester(name='tester_1', deadline=False)
-tester_1.work_hard()
+tester_1 = Tester(name='tester_1')
+tester_1.work_hard(deadline=False)
 tester_2 = Tester(name='tester_2')
-tester_2.work_hard()
+tester_2.work_hard(deadline=True)
